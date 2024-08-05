35eba13313efde13e63fbcf127baae3520fa752b
Cookie <3687787@gmail.com>
2024-08-05 13:11:43 +0800

    chore(emby2Alist): 修改配置

--- emby2Alist/nginx/conf.d/config/constant-mount.js
+++ emby2Alist/nginx/conf.d/config/constant-mount.js
@@ -6,10 +6,10 @@ const strHead = commonConfig.strHead;
 
 // rclone/CD2 挂载的 alist 文件配置,根据实际情况修改下面的设置
 // 访问宿主机上 5244 端口的 alist 地址, 要注意 iptables 给容器放行端口
-const alistAddr = "http://172.17.0.1:5244";
+const alistAddr = "http://172.17.0.4:5244";
 
 // alist token, 在 alist 后台查看
-const alistToken = "alsit-123456";
+const alistToken = "alist-1111";
 
 // alist 是否启用了 sign
 const alistSignEnable = false;
@@ -18,7 +18,7 @@ const alistSignEnable = false;
 const alistSignExpireTime = 12;
 
 // alist 公网地址,用于需要 alist server 代理流量的情况,按需填写
-const alistPublicAddr = "http://youralist.com:5244";
+const alistPublicAddr = "http://x.x.x.x:5244";
 
 // 指定客户端自己请求并获取 alist 直链的规则,代码优先级在 redirectStrmLastLinkRule 之后
 // 特殊情况使用,则此处必须使用域名且公网畅通,用不着请保持默认
@@ -31,7 +31,7 @@ const alistPublicAddr = "http://youralist.com:5244";
 const clientSelfAlistRule = [
   // "Emby for iOS"和"Infuse"对于 115 的进度条拖动依赖于此
   // 如果 nginx 为 https,则此 alist 也必须 https,浏览器行为客户端会阻止非 https 请求
-  [2, strHead["115"], alistPublicAddr],
+  // [2, strHead["115"], alistPublicAddr],
   // [2, strHead.ali, alistPublicAddr],
   // 优先使用 filePath,可省去一次查询 alist,如驱动为 alias,则应使用 alistRes
   // ["115-local", "filePath", 0, "/mnt/115", alistPublicAddr],
